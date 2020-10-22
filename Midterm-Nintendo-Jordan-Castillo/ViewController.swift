1d3e8161f56d9b6f1c18da30172fb0d2903acc50
Jordan Castillo <jordannng@yahoo.com>
2020-10-22 13:58:32 -0700

    Initial Commit

---
+++ Midterm-Nintendo-Jordan-Castillo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Midterm-Nintendo-Jordan-Castillo
+//
+//  Created by Jordan Castillo on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Midterm-Nintendo-Jordan-CastilloTests/Midterm_Nintendo_Jordan_CastilloTests.swift
@@ -0,0 +1,33 @@
+//
+//  Midterm_Nintendo_Jordan_CastilloTests.swift
+//  Midterm-Nintendo-Jordan-CastilloTests
+//
+//  Created by Jordan Castillo on 10/22/20.
+//
+
+import XCTest
+@testable import Midterm_Nintendo_Jordan_Castillo
+
+class Midterm_Nintendo_Jordan_CastilloTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
